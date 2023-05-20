00439b34fe07e27c9832151274b1b28974b5f762
RUZIGOLLA <nicolas.ruiz1@epsi.fr>
2023-05-21 01:13:16 +0200

    href mechanic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import puppeteer from "puppeteer";
 
-async function Run() {
-  var city = "lyon";
+async function Run({ city = "marseille"}) {
   var prompt = "evenements " + city;
   // Instancier le navigateur
   const browser = await puppeteer.launch({ headless: false });
@@ -10,42 +9,13 @@ async function Run() {
   // Redimentionner la page dans le navigateur
   await page.setViewport({ width: 2000, height: 1024 });
   // Ouvrir la page dans le navigateur
-  await page.goto("https://www.google.fr/search?q=evenements+lyon");
+  await page.goto("https://www.google.fr/search?q=evenements+" + city);
 
   // Wait and click on first result
   const searchResultSelector = "#L2AGLb";
   await page.waitForSelector(searchResultSelector);
   await page.click(searchResultSelector);
 
-  //   try {
-  //     // Attendre que l'élément avec la classe "ct5Ked" soit présent dans la page
-
-  //     await page.waitForSelector(".MJfuMd", { visible: true, timeout: 5000 });
-
-  //     // Sélectionner les éléments avec la classe "ct5Ked"
-  //     const elements = await page.$$(".MJfuMd");
-
-  //     console.log(elements);
-  //   } catch (error) {
-  //     console.error("Erreur lors de l'attente de l'élément .MJfuMd :", error);
-  //   }
-
-  //   try {
-  //     // Attendre que les éléments soient chargés
-  //     await page.waitForXPath('//div[contains(@class, "MJfuMd")]', {
-  //       visible: true,
-  //       timeout: 5000,
-  //     });
-
-  //     // Récupérer les éléments avec une expression XPath
-  //     const elements = await page.$x('//div[contains(@class, "MJfuMd")]');
-
-  //     console.log(elements);
-  //   } catch (error) {
-  //     console.error("Erreur lors de l'attente des éléments .vrdfdd :", error);
-  //   }
-
-  // Récupérer tous les liens (balises <a>) de la page
   const titles = await page.$$("div.bVj5Zb");
   const adress = await page.$$("div.TCYkdd");
   const dates = await page.$$("div.t3gkGd");
@@ -53,28 +23,53 @@ async function Run() {
   const len = titles.length;
 
   // Afficher le texte et l'URL de chaque lien
+  // const eventsHref = await page.$$("a.ct5Ked");
+  // for (let i = 0; i < len; i++) {
+  //   const url = await eventsHref[i].evaluate((node) => node.href);
+  //   console.log(url);
+  // }
+    const eventsHref = await page.$$("a.ct5Ked");
+    const eventsHrefArray = await Promise.all(
+      eventsHref.map((link) => link.evaluate((node) => node.href))
+    );
+  console.log(eventsHrefArray);
+  console.log(eventsHrefArray.length);
+  console.log("🚀 ~ file: index.ts:59 ~ Run ~ eventsHref:", eventsHref)
+
 
   for (let i = 0; i < len; i++) {
-    const titles = await page.$$("div.bVj5Zb");
-    const adress = await page.$$("div.TCYkdd");
-    const dates = await page.$$("div.t3gkGd");
-    console.log(
-      await titles[i].evaluate((node) => node.textContent),
-      await adress[i].evaluate((node) => node.textContent),
-      await dates[i].evaluate((node) => node.textContent)
-    );
-    console.log(titles[i]);
+    page.goto(eventsHrefArray[i]);
     // titles[i].click();
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    try {
+
+    
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const data = await page.evaluate(() => {
+      const title = document.querySelector('.vk_h')?.textContent;
+      const date = document.querySelector('.vk_sh span')?.textContent;
+      const organiser = document.querySelector('.VF0Pj VECn4b div a')?.textContent;
+      const address = document.querySelector('.VF0Pj VECn4b .vk_gy')?.textContent;
+      const url = document.querySelector('.VECn4b a')?.textContent;
+
+      return {
+          title,
+          date,
+          organiser,
+          address,
+          url,
+      };
+  });
+  console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  
   }
-  //   for (const title of titles) {
-
-  //     const text = await link.evaluate((node) => node.textContent);
-  //     // const url = await link.evaluate((node) => node.href);
-  //     console.log(text);
-  //   }
-
-  // console.log(JSON.stringify(container[0]));
+ catch (error) {
+  console.log("🚀 ~ file: index.ts:67 ~ Run ~ error:", error)
+  
 }
 
-Run();
+
+  // console.log(JSON.stringify(container[0]));
+}}
+Run({});
